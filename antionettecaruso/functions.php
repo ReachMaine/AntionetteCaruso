203ebe776c86b004442d1af26956efb3a7065d1b
Linda "zig" Speight <linda@reachmaine.com>
2018-03-12 17:08:17 -0400

    playing with footer and gravity forms

--- antionettecaruso/functions.php
+++ antionettecaruso/functions.php
@@ -2,7 +2,7 @@
 //
 // Recommended way to include parent theme styles.
 //  (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
-//  
+//
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -14,3 +14,5 @@ function theme_enqueue_styles() {
 //
 // Your code goes below
 //
+// allow ability to hide field labels
+add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
